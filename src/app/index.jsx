74fdedaa61a448e0b3282d4e22b73aa0a484eb9d
恪晨 <bo.wang1016@outlook.com>
2020-08-13 19:28:39 +0800

    :tada: init project

---
+++ src/app/index.jsx
@@ -0,0 +1,12 @@
+import React  from 'react';
+
+function App() {
+
+    return (
+        <div className="App">
+            hello，this is a vite react demo
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from '@pika/react';
+import ReactDOM from '@pika/react-dom';
+import App from './app';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
