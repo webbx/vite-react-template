4a5670c6d6f54525d44de8b7775edfd0ed6a0cee
恪晨 <bo.wang1016@outlook.com>
2020-12-25 15:48:17 +0800

    add ant design UI

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
 
     return (
         <div className="App">
             hello，this is a vite react demo
+			<br/>
+			{<Button>你好,我是Ant Design的Button组件</Button>}
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from '@pika/react';
 import ReactDOM from '@pika/react-dom';
 import App from './app';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
     <App />,
