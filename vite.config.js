53d0b67026cc1b95d18eb966a4442cfe9740eccb
恪晨 <bo.wang1016@outlook.com>
2021-02-18 23:35:43 +0800

    update project

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-const path = require('path');
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
-module.exports = {
-	jsx: 'react',
-	// 定义别名
-	alias: {
-		// 路径映射必须以/开头和结尾
-		"/components/": path.resolve(__dirname, "src/components"),
-		"/css/": path.resolve(__dirname, "src/assets/css"),
-	},
-
-	plugins: [require('vite-plugin-react'), require('autoprefixer'),]
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()]
+})
